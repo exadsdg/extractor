4b5ba69f3f1190ba1177121772596ed44b48862a
exadsdg <161360920+exadsdg@users.noreply.github.com>
2025-09-02 11:18:28 -0300

    Segunda Parte

---
+++ index_page.js
@@ -0,0 +1,11 @@
+var page = require('webpage').create(),
+system = require('system');
+
+
+
+page.onLoadFinished = function() {
+	
+	console.log(page.content);
+	phantom.exit();
+};
+page.open(system.args[1]);
